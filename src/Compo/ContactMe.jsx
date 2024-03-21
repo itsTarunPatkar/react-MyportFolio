c85a6de27dd44dd156f50287f156d93eebabf714
Tarun Kumar Patkar <tarunpatkar530@gmail.com>
2024-03-21 10:00:50 +0530

    added hover effect in contact icons

--- src/Compo/ContactMe.jsx
+++ src/Compo/ContactMe.jsx
@@ -10,10 +10,9 @@ export default function ContactMe() {
   const [email, setEmail] = useState('');
   const [Text, setText] = useState('');
   const [showSendToTarun, setShowSendToTarun] = useState(false);
-
+  
   const handleSubmit = (e) => {
     e.preventDefault();
-
     if (name && email && Text) {
       setDoc(doc(collection(db, 'Contact')), {
         name: name,
@@ -35,13 +34,8 @@ export default function ContactMe() {
       alert('Please fill in all fields');
     }
   };
-
   // Check if all three fields have values to determine whether to show "Send it to me" button
   const shouldShowSendButton = name && email && Text;
-
-
-
-
 
   return (
     <><div className="smooth">
@@ -56,10 +50,6 @@ export default function ContactMe() {
 
         <Row>
           <Col md={3}>
-
-
-
-
           </Col>
           <Col md={6} sm={12} className="d-flex flex-column align-items-center InputGroup">
             <InputGroup className="mb-3">
@@ -82,6 +72,7 @@ export default function ContactMe() {
                 onChange={(e) => setEmail(e.target.value)}
               />
             </InputGroup>
+            
             <InputGroup>
               <Form.Control
                 className="mb-3"
@@ -104,61 +95,39 @@ export default function ContactMe() {
             )}
           </Col>
         </Row>
-
         <Row>
 
-
           <footer className="text-center text-white" style={{}}>
-
-
-
             <div className="container d-flex flex-column">
-
-
-
-
               <section class=" mt-5 container d-flex flex-column ">
                 <div class="row d-flex justify-content-center">
                   <div class="col-lg-8">
                     <p>
                       "Thank you for exploring my portfolio. If you'd like to connect, discuss opportunities, or explore potential collaborations, please feel free to reach out. I'm always open to new ventures and partnerships that can contribute to our shared success. Let's connect and make things happen."
-
                     </p>
                   </div>
                 </div>
               </section>
-
-
               <hr className="my-5" />
-
               <section className="d-flex justify-content-center  mb-5" >
-
                 <a href="https://www.instagram.com/world.tarun/" style={{ fontSize: "" }} className="text-white me-4" >
-                  <i style={{ fontSize: "3rem" }} className="fab fa-instagram"></i>
+                  <i style={{ fontSize: "3rem" }} className="fab fa-instagram contactIcons"></i>
                 </a>
                 <a href="https://www.linkedin.com/in/tarunpatkar" className="text-white me-4">
-                  <i style={{ fontSize: "3rem" }} className="fab fa-linkedin"></i>
+                  <i style={{ fontSize: "3rem" }} className="fab fa-linkedin contactIcons"></i>
                 </a>
                 <a href="https://github.com/itsTarunPatkar" className="text-white me-4">
-                  <i style={{ fontSize: "3rem" }} className="fab fa-github"></i>
+                  <i style={{ fontSize: "3rem" }} className="fab fa-github contactIcons" ></i>
                 </a>
               </section>
-
             </div>
-
-
-
             <div
               className="text-center p-3 mb-1"
               style={{ background: "transparent", }}
             >
-              © 2023 Copyright: Tarun Kumar Patkar
-
-
+              © 2024 Copyright: Tarun Kumar Patkar
             </div>
-
           </footer>
-
         </Row>
       </Container>
     </div>
